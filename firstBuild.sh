6a02fd76300fedbef0cc56866c30f4b0bdb50117
plinux <plinux@plinux-MacBookPro>
2016-10-11 17:03:27 +0200

    Working scripts - and structured project . v2

--- firstBuild.sh
+++ firstBuild.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
+
+# save user
+#let me=$(whoami)
+#echo $me
+
 # Get root
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
+#if [ $EUID != 0 ]; then
+#    sudo "$0" "$@"
+#    exit $?
+#fi
 
 # remove old build and devel
-sudo rm -rf devel build ./src/CMakeLists.txt
+#sudo rm -rf devel build ./src/CMakeLists.txt
+
+# switch to user mode
+#su $me
+#sudo su
 
 # init workspace
-cd src && catkin_init_workspace
+cd ./src/ && catkin_init_workspace 
 
 # build driver_base; timestamp_tools; driver_common packages first
+cd ..
 catkin_make -DCATKIN_WHILELIST_PACKAGES="driver_base; timestamp_tools; driver_common"
 
 # build all
 catkin_make -DCATKIN_WHILELIST_PACKAGES=""
+
+echo "Done! :-)"
+
