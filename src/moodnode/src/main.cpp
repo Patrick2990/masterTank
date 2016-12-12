883de411b4b5e0ef4d490494267577f81ac7390a
Hippomormor <cgenter@oncable.dk>
2016-12-12 10:49:37 +0100

    Added moodnode

---
+++ src/moodnode/src/main.cpp
@@ -0,0 +1,14 @@
+#include "Sound.h"
+
+int main(int argc, char **argv) {
+
+    std::cout << "Starting node.." << std::endl;
+    ros::init(argc, argv, "moodnode");
+
+    ros::NodeHandle nh;
+
+    Sound * soundTopicHandler = new Sound();
+    soundTopicHandler->startSoundTopic(nh);
+
+    return 0;
+}
